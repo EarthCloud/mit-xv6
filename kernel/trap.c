28617881312187135e2c1afa9e43cb1307a4aec3
earthcloud <earth_zhangyunyang@outlook.com>
2026-03-08 23:35:21 +0800

    [lazy allocation] trap: check page fault and do the allocation                   vm: modify uvmunmap to skip epmty page

--- kernel/trap.c
+++ kernel/trap.c
@@ -65,6 +65,14 @@ usertrap(void)
     intr_on();
 
     syscall();
+  } else if(r_scause() == 13 || r_scause() == 15) {
+    uint64 addr = r_stval();
+    if(addr > p->sz) {
+      printf("usertrap(): unexpected scause %p pid=%d\n", r_scause(), p->pid);
+      printf("            sepc=%p stval=%p\n", r_sepc(), r_stval());
+      p->killed = 1;
+    }
+    uvmalloc(p->pagetable, PGROUNDDOWN(addr), addr);
   } else if((which_dev = devintr()) != 0) {
     // ok
   } else {
