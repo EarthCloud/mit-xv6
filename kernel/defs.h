fce22720a2f34ccc950a179a2d54a86d345c26a2
earthcloud <earth_zhangyunyang@outlook.com>
2026-03-11 20:22:59 +0800

    kalloc: import ref count array and modified kfree/kalloc

--- kernel/defs.h
+++ kernel/defs.h
@@ -63,6 +63,7 @@ void ramdiskrw(struct buf *);
 void *kalloc(void);
 void  kfree(void *);
 void  kinit(void);
+void  kaddref(void *pa);
 
 // log.c
 void initlog(int, struct superblock *);
--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -42,8 +42,11 @@ freerange(void *pa_start, void *pa_end)
 {
   char *p;
   p = (char *)PGROUNDUP((uint64)pa_start);
-  for(; p + PGSIZE <= (char *)pa_end; p += PGSIZE)
+  for(; p + PGSIZE <= (char *)pa_end; p += PGSIZE) {
+    // add a count to keep kfree init safe
+    kaddref(p);
     kfree(p);
+  }
 }
 
 // Free the page of physical memory pointed at by v,
@@ -56,8 +59,17 @@ kfree(void *pa)
   struct run *r;
 
   if(((uint64)pa % PGSIZE) != 0 || (char *)pa < end || (uint64)pa >= PHYSTOP)
-    panic("kfree");
+    panic("kfree: invalid address");
 
+  acquire(&ref.lock);
+  if(ref.count[(uint64)pa / PGSIZE] <= 0) {
+    panic("kfree: invalid free");
+  }
+  int current_count = --ref.count[(uint64)pa / PGSIZE];
+  release(&ref.lock);
+  if(current_count > 0) {
+    return;
+  }
   // Fill with junk to catch dangling refs.
   memset(pa, 1, PGSIZE);
 
@@ -83,14 +95,19 @@ kalloc(void)
     kmem.freelist = r->next;
   release(&kmem.lock);
 
-  if(r)
+  if(r) {
     memset((char *)r, 5, PGSIZE); // fill with junk
+    kaddref(r);
+  }
   return (void *)r;
 }
 
 void
 kaddref(void *pa)
 {
+  if(((uint64)pa % PGSIZE) != 0 || (char *)pa < end || (uint64)pa >= PHYSTOP)
+    panic("kaddref: invalid address");
+
   int idx = (uint64)pa / PGSIZE;
   // add must be locked
   acquire(&ref.lock);
